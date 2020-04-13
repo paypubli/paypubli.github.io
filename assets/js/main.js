b57deb91b2cf029fc8031cb4aed64d5b9126d7ac
Willian Dutras <willian_dutras@hotmail.com>
2020-04-13 20:48:08 -0300

    first

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault (); var target = this.hash,
+    $target = $(target); $('html, body').stop().animate ({
+      'scrollTop': $target.offset().top
+    }, 1000, 'swing', function () {
+        window.location.hash = target;
+    });
+  });
+
+
+  var nav = $('.top');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            nav.addClass("fixed");
+        } else {
+            nav.removeClass("fixed");
+        }
+    });
+
+});
---
+++ segredos-do-ifood/assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  const ano = new Date();
+
+  $('#ano').html(ano.getFullYear());
+
+  $("input[name='PHONE']").inputmask("99 - 99999-9999");
+});
