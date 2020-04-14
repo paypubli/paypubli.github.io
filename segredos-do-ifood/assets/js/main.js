ce946a761714552dc20946b4b67b3849e7d6f381
Willian Dutras <willian_dutras@hotmail.com>
2020-04-14 00:50:32 -0300

    updates

--- segredos-do-ifood/assets/js/main.js
+++ segredos-do-ifood/assets/js/main.js
@@ -5,23 +5,4 @@ $(document).ready(function() {
 
   $("input[name='PHONE']").inputmask("99 - 99999-9999");
 
-  $('#mc-embedded-subscribe-form').submit(function (e) {
-      e.preventDefault();
-      $.ajax({
-          url: 'https://paypubli.us19.list-manage.com/subscribe/post?u=4fb25629572c3c820d598d597&amp;id=5ba34d2f45',
-          type: 'GET',
-          data: $('#mc-embedded-subscribe-form').serialize(),
-          dataType: 'jsonp',
-          contentType: "application/json; charset=utf-8",
-          success: function (data) {
-             if (data['result'] != "success") {
-
-                  console.log(data['msg']);
-             } else {
-                  //SUCCESS - Do what you like here
-                  window.location = 'https://paypubli.github.io/segredos-do-ifood/v1/';
-             }
-          }
-      });
-  });
 });
